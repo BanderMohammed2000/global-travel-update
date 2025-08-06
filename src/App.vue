a12e9b06e5212734b8f7d3702d76ce5152b35855
Bander Btati <bander.mohammed.2000@gmail.com>
2025-08-06 03:27:03 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script></script>
+
+<template>
+  <div
+    class="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 text-white text-3xl font-bold"
+  >
+    Tailwind is working!
+  </div>
+</template>
+
+<style scoped></style>
